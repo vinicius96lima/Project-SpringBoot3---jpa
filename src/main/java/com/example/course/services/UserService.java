f7495006b563c9c72b56e54684ef443c10bc4d28
vinicius96lima <viniciusaraujo_96@hotmail.com>
2023-04-17 12:12:15 -0300

    Exception update

--- src/main/java/com/example/course/services/UserService.java
+++ src/main/java/com/example/course/services/UserService.java
@@ -13,6 +13,8 @@ import com.example.course.repositories.UserRepository;
 import com.example.course.services.exceptions.DatabaseException;
 import com.example.course.services.exceptions.ResourceNotFoundException;
 
+import jakarta.persistence.EntityNotFoundException;
+
 
 // @Component // Anotação que serve para registro no nosso Spring,para ser utilizado em outras classes.
 // @Repository // Anotação para registrar repositorio
@@ -50,9 +52,15 @@ public class UserService {
 	}
 	
 	public User update (Long id, User obj) {
-		User entity = repository.getReferenceById(id);
-		updateData(entity, obj);
-		return repository.save(entity);
+		try{
+			User entity = repository.getReferenceById(id);
+			updateData(entity, obj);
+			return repository.save(entity);
+		}
+		catch (EntityNotFoundException e) {
+			throw new ResourceNotFoundException(id);
+		}
+		
 	}
 
 	private void updateData(User entity, User obj) {
